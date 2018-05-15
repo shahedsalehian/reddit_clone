dedcb5d67732c1fa0b1fa4d2e677a9cb699fd616
shahedsalehian <shahed.salehian.805@my.csun.edu>
2018-05-15 15:10:52 -0700

    replies to comments work and are displayed; todo: reply form link form comment

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-//used to access currentUser from all templates/views
+//used to access request vars from all templates/views
 app.use(function(req,res,next){
   res.locals.currentUser = req.user;
   res.locals.error       = req.flash("error");
