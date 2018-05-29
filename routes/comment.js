65726a0bdab8f65fbaaba0c03aa7d0d7b42fd805
shahedsalehian <shahed.salehian.805@my.csun.edu>
2018-05-29 16:53:06 -0700

    add delete comment route

--- routes/comment.js
+++ routes/comment.js
@@ -33,6 +33,42 @@ router.post("/",middleware.isLoggedIn,function(req,res){
   });
 });
 
+//EDIT COMMENT
+router.get("/:comment_id/edit", middleware.isLoggedIn, function(req,res){
+  Comment.findById(req.params.comment_id, function(err, comment){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("comments/edit", {sub: req.param.id, post: req.params.post_id, comment: comment});
+    }
+  });
+});
+ 
+//UPDATE COMMENT
+router.put("/:post_id", middleware.isLoggedIn, function(req,res){
+  Comment.findByIdAndUpdate(req.params.post_id, req.body.post, function(err,post){
+    if(err){
+      console.log(err);
+      res.redirect("/s");
+    }else{
+      res.redirect("/s/" + sub.id +"/posts/" + req.params.post_id);
+    }
+  });
+});
+
+
+
+//DELETE COMMENT
+router.delete("/:comment_id", middleware.isLoggedIn, function(req,res){
+  Comment.findByIdAndRemove(req.params.comment_id, function(err, comment){
+    if(err){
+      console.log(err);
+      res.redirect(`/s/${req.params.id}/posts/${req.params.post_id}`);
+    }else{
+      res.redirect(`/s/${req.params.id}/posts/${req.params.post_id}`);
+    }
+  });
+});
 
 
 module.exports = router;
